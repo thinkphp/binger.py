a0868eb037226f02c37902b75c7b1e4801d88eb6
Adrian Statescu <mergesortv@gmail.com>
2013-03-15 00:24:23 -0700

    xml results formt

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 A simple client for Microsoft's Bing API Search.
 
+# Install
+
+```
+$ git clone git://github.com/thinkphp/binger.py.git
+$ cd binger.py
+$ python app.py
+$ => output
+```
+
 # Usage
 
+Results as JSON
 ```
 bing = Bing('your-api-key')
 
@@ -11,6 +21,15 @@ print bing.search(query='MooTools',sources='web')
 print bing.search(query='jQuery',sources='web image')
 print bing.search(query='Dojo',sources='news')
 
+```
+
+Results as XML
+
+```
+bing = Bing('49EB4B94127F7C7836C96DEB3F2CD8A6D12BDB71','xml')
+
+print bing.search(query='mootools',sources='web')
+# => xml.dom.minidom.Document instance at 0x00D4F5A8>
 ```
 
 # License
--- lib/bing.py
+++ lib/bing.py
@@ -1,19 +1,37 @@
 import urllib
 import urllib2
 import json
+from xml.dom.minidom import parse, parseString
+
 
 class Bing:
-      ENDPOINT = 'http://api.bing.net/json.aspx'
 
-      def __init__(self, appid):
+      ENDPOINT = 'http://api.bing.net/format.aspx'
+
+      def __init__(self, appid, format='json'):
+
           self.appid = appid
+          self.format = format
 
       def search(self,**params):
+ 
+          self.ENDPOINT = self.ENDPOINT.replace('format',self.format)
+
           data = {}
+
           for k,v in params.items():
+
               data.update({k.replace('_','.'):v.replace(' ','+')})
+
           qs = urllib.urlencode(data).replace('%2B','+')
+
           url = '%s?Appid=%s&%s' % (self.ENDPOINT, self.appid, qs) 
+
           f = urllib2.urlopen(url)
-          return json.loads(f.read())
+
+          if self.format == 'json':
+             return json.loads(f.read())
+          else:
+             return parse(f)
+
   
